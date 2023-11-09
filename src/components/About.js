65086c6349bfca832ba14efadd5afd1f0e0415f0
Shreyash Dhande <shreyashdhande346@gmail.com>
2023-11-09 22:54:27 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About</h2>
+      <p>Hey this is me, Shreyash Dhande 21bce5705.</p>
+      <p>This is the task 6 of react under Ethnus MERN Internship</p>
+    </section>
+  );
+}
+
+export default About;
